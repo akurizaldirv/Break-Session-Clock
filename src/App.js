c85cf3699afed139a79ea2fbffcb66cd2ff8fd65
akurizaldirv <akurizaldirv@gmail.com>
2023-12-17 19:20:31 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft, faCirclePause, faCirclePlay, faCircleStop } from '@fortawesome/free-solid-svg-icons';
+import ClockSessionBreak from "./ClockSessionBreak";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClockSessionBreak />
     </div>
   );
 }
